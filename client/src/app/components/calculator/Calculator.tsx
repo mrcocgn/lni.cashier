e60ab3ee528a12a3c1a34ea19c32fa36b058bc41
mrcocgn <marcos.ilgi@gmail.com>
2025-05-10 20:20:32 +0200

    feat: enhance calculator functionality and fix font path in CSS

--- client/src/app/components/calculator/Calculator.tsx
+++ client/src/app/components/calculator/Calculator.tsx
@@ -1,31 +1,57 @@
 import "./Calculator.css";
+import { useState } from "react";
 
 function Calculator() {
+  const [input, setInput] = useState("");
+
+  const handleClick = (value: string) => {
+    switch (value) {
+      case "AC":
+        setInput("");
+        break;
+      case "C":
+        setInput(prev => prev.slice(0, -1));
+        break;
+      case "Enter / =":
+        try {
+          setInput(eval(input).toString());
+        } catch {
+          setInput("Error");
+        }
+        break;
+      case "X":
+        setInput(prev => prev.slice(0, -1));
+        break;
+      case "Products":
+      case "Services":
+        alert(`TODO: Open ${value} panel`);
+        break;
+      default:
+        setInput(prev => prev + value);
+        break;
+    }
+  };
+
+  const buttons = [
+    "7", "8", "9", "*", "%", "X",
+    "4", "5", "6", "-", "C", "AC",
+    "1", "2", "3", "+", "Products", "Services",
+    "0", "00", ".", "Enter / ="
+  ];
+
   return (
     <div className="calculator">
+      <div className="display">{input}</div>
       <div className="parent">
-        <div className="div1 children">7</div>
-        <div className="div2 children">8</div>
-        <div className="div3 children">9</div>
-        <div className="div4 children"><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="black"><path d="M444-144v-249L268-217l-51-51 176-176H144v-72h249L217-692l51-51 176 176v-249h72v249l176-176 51 51-176 176h249v72H567l176 176-51 51-176-176v249h-72Z"/></svg></div>
-        <div className="div5 children"><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="black"><path d="M311.8-528q-49.8 0-84.8-35.2t-35-85q0-49.8 35.2-84.8t85-35q49.8 0 84.8 35.2t35 85q0 49.8-35.2 84.8t-85 35Zm.2-72q20 0 34-14t14-34q0-20-14-34t-34-14q-20 0-34 14t-14 34q0 20 14 34t34 14Zm335.8 408q-49.8 0-84.8-35.2t-35-85q0-49.8 35.2-84.8t85-35q49.8 0 84.8 35.2t35 85q0 49.8-35.2 84.8t-85 35Zm.2-72q20 0 34-14t14-34q0-20-14-34t-34-14q-20 0-34 14t-14 34q0 20 14 34t34 14Zm-405 72-51-51 525-525 51 51-525 525Z"/></svg></div>
-        <div className="div6 children"><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="black"><path d="m339-288 141-141 141 141 51-51-141-141 141-141-51-51-141 141-141-141-51 51 141 141-141 141 51 51ZM480-96q-79 0-149-30t-122.5-82.5Q156-261 126-331T96-480q0-80 30-149.5t82.5-122Q261-804 331-834t149-30q80 0 149.5 30t122 82.5Q804-699 834-629.5T864-480q0 79-30 149t-82.5 122.5Q699-156 629.5-126T480-96Zm0-72q130 0 221-91t91-221q0-130-91-221t-221-91q-130 0-221 91t-91 221q0 130 91 221t221 91Zm0-312Z"/></svg></div>
-        <div className="div7 children">4</div>
-        <div className="div8 children">5</div>
-        <div className="div9 children">6</div>
-        <div className="div10 children"><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="black"><path d="M232-444v-72h496v72H232Z"/></svg></div>
-        <div className="div11 children">C</div>
-        <div className="div12 children">AC</div>
-        <div className="div13 children">1</div>
-        <div className="div14 children">2</div>
-        <div className="div15 children">3</div>
-        <div className="div16 children"><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="black"><path d="M444-444H240v-72h204v-204h72v204h204v72H516v204h-72v-204Z"/></svg></div>
-        <div className="div17 children">Products</div>
-        <div className="div18 children">Services</div>
-        <div className="div19 children">0</div>
-        <div className="div20 children">00</div>
-        <div className="div21 children">.</div>
-        <div className="div22 children"><svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="black"><path d="M400-120 160-360l241-241 56 57-144 144h367v-400h80v480H313l144 143-57 57Z"/></svg></div>
+        {buttons.map((label, index) => (
+          <div
+            key={index}
+            className={`div${index + 1} children`}
+            onClick={() => handleClick(label)}
+          >
+            {label}
+          </div>
+        ))}
       </div>
     </div>
   );
